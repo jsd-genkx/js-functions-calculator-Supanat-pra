b6f4cc2d9733587676493adef5cc6dfcdb3e1a82
Supanat-pra <earthpra06@gmail.com>
2025-07-16 18:28:20 +0700

    calculator solution

--- calculator.js
+++ calculator.js
@@ -1,7 +1,53 @@
 "use strict";
 
 // Step 01: Create Functions for Addition, Subtraction, Multiplication, and Division
+function add(num1,num2) {
+    return num1 + num2;
+};
 
+function substract(num1,num2) {
+    return num1 - num2;
+};
+
+function multiply(num1,num2) {
+    return num1 * num2;
+};
+
+function divide(num1,num2) {
+    if (num2 === 0) {return "Cannot divide by zero";}
+    return num1 / num2;
+};
 // Step 02: Create a Validation Function for Inputs
+function validateInputs(num1,num2) {
+    if (typeof num1 === "number" && typeof num2 === "number" && typeof num1 !== NaN && typeof num2 !== NaN) {
+        return null;
+    }
+    else {
+        return "Both inputs must be numbers";
+    };
+};
 
 // Step 03: Create a Calculator Function to Combine the Above Functions
+function calculator(num1,num2,operation) {
+    if (operation === "add")
+        return add(num1,num2);
+    else if (operation === "substract" ) {
+        return substract(num1,num2);
+    }
+    else if (operation === "multiply" ) {
+        return multiply(num1,num2);
+    }
+    else if (operation === "divide" ) {
+        return divide(num1,num2);
+    }
+    else {
+        return "Invalid operation. Please use 'add', 'subtract', 'multiply', or 'divide'";
+    };
+};
+
+console.log(add(10,5));
+console.log(validateInputs(10, "a"));
+console.log(calculator(10, 5, "add"));
+console.log(calculator(10, 5, "divide"));
+console.log(calculator(10, 0, "divide"));
+console.log(calculator(10, 5, "unknown"))
